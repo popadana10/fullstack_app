196a64511490849c87b2339d570f177c0f4c2518
Dana Popa <popa.dana100@gmail.com>
2024-04-09 13:17:43 +0300

    form css

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateUser = () => {
+    const[inputs, setInputs] = useState({}); 
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values) =>({...values, [name]: value}));
+    
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8005/api/', inputs);
+        console.log(inputs);
+    };
+
   return (
-    <div>
+    <div className='container'>
       <h2>Create User</h2>
-      <form>
+      <form onSubmit={handleSubmit} className='form-group'>
         <label>Name: </label>
-        <input type="text" name="name"/>
-        <br />
+        <input type="text" name="name" onChange={handleChange}/>
         <label>Email: </label>
-        <input type="text" name="email"/>
-        <br />
+        <input type="text" name="email" onChange={handleChange}/>
         <label>Mobile: </label>
-        <input type="text" name="mobile"/>
+        <input type="text" name="mobile" onChange={handleChange}/>
         <br />
-        <button>Save</button>
+        <button className='button'>Save</button>
       </form>
     </div>
   );
